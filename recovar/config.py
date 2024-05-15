5b67ab90d3958f606d8cfc9d31d01ec83dfb31cd
Marc A. Gilles <mg6942@princeton.com>
2024-05-15 13:16:00 -0400

    Added missing file

--- recovar/config.py
+++ recovar/config.py
@@ -23,11 +23,17 @@ logging.config.dictConfig(
 )
 
 logger = logging.getLogger(__name__)
-import tensorflow as tf
-tf.config.experimental.set_visible_devices([], "GPU")
 import os
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"  # add this
 os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = ".90"
+
+# os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"]="false"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"]=".75"
+# os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"]="platform"
+
+import tensorflow as tf
+tf.config.experimental.set_visible_devices([], "GPU")
+
 import jax
 jax.config.update("jax_enable_x64", True)
 # Interestingly, nothing works if I don't do this print statement :)))) 
