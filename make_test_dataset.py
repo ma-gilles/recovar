3813b76fdd9bf75fff7adc6282a4079769ac0861
Marc A. Gilles <mg6942@princeton.com>
2024-07-23 16:21:34 -0400

    ewald stuff

--- make_test_dataset.py
+++ make_test_dataset.py
@@ -9,7 +9,7 @@ import os
 #     atom_coeffs = json.load(f)
 
 
-def make_test_dataset():
+def make_test_dataset(noise_level = 0.1, n_images = None):
     grid_size =64
     this_dir = os.path.dirname(__file__)
     volume_folder_input =  this_dir+ '/recovar/data/vol'
@@ -19,13 +19,13 @@ def make_test_dataset():
 
     outlier_file_input = None
     log_n = 3
-    n_images = int(10**(log_n))
+    n_images = int(10**(log_n)) if n_images is None else n_images
     voxel_size = 4.25 * 128 / grid_size 
 
     volume_distribution = np.array([1/4, 1/4, 1/2])
     image_stack, sim_info = simulator.generate_synthetic_dataset(output_folder, voxel_size, volume_folder_input, n_images,
                                      outlier_file_input = outlier_file_input, grid_size = grid_size,
-                                volume_distribution = volume_distribution,  dataset_params_option = "uniform", noise_level = 0.1,
+                                volume_distribution = volume_distribution,  dataset_params_option = "uniform", noise_level =noise_level,
                                 noise_model = "radial1", put_extra_particles = False, percent_outliers = 0.00, 
                                 volume_radius = 0.7, trailing_zero_format_in_vol_name = True, noise_scale_std = 0.2 * 0, contrast_std =0.1   , disc_type = 'linear_interp')
     
