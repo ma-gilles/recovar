1120e05274195912d75042bdf5e80d2f461e60a9
Marc A. Gilles <mg6942@princeton.com>
2025-03-04 22:08:17 -0500

    idk

--- recovar/_version.py
+++ recovar/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev216+g3c40b12'
-__version_tuple__ = version_tuple = (0, 1, 'dev216', 'g3c40b12')
+__version__ = version = '0.4.2.dev0'
+__version_tuple__ = version_tuple = (0, 4, 2, 'dev0')
